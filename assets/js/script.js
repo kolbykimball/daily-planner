c34107057bda00da3e257b1af717c3deda6fe217
Kolby Kimball <Kolby@Mac-mini.local>
2022-07-25 10:40:33 -0600

    for loop, save reminder, button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,18 @@ $("#currentDay").text(today.format("MMM Do, YYYY, h:mm:ss a"));
 
 // compare current time to time block - change background color of textarea to a certain color pertaining to past present or future - use classlist.add (check with jquery as well).
 
+var workDay = localStorage.getItem("workDay") || {
+    9:"",
+    10:"",
+    11:"",
+    12:"",
+    13:"",
+    14:"",
+    15:"",
+    16:"",
+    17:"",
+}
+
 var currentHour = moment().format("H")
 var textarea = document.getElementsByTagName("textarea")
 console.log(textarea);
@@ -12,14 +24,20 @@ for (let index = 0; index < textarea.length; index++) {
     var textareaHour = textarea[index].dataset.hour;
     if (textareaHour == currentHour) {
         // add present class to this textarea 
-        classlist.add("present");
+        textarea[index].classList.add("present");
     } else if(textareaHour > currentHour) {
         // add future class 
-        classlist.add("future");
+        textarea[index].classList.add("future");
     } else {
         // add past class
-        classlist.add("past");
+        textarea[index].classList.add("past");
     }
+    
+}
+var allButtons = document.getElementsByTagName("button")
+for (let index = 0; index < allButtons.length; index++) {
+    const element = allButtons[index];
+    element.addEventListener("click", saveReminder);
     
 }
 // when save button is clicked, data in textarea is saved to local storage
@@ -30,15 +48,31 @@ for (let index = 0; index < textarea.length; index++) {
     // save this value in the local storage (key name that makes sense) specific data-hour 
     // localStorage.setItem("key here", "value")
 
+function saveReminder(event) {
+    var buttonClicked = event.target;
+    console.log(buttonClicked);
+    // declaring variable to track index of button clicked
+    var buttonIndex = 0;
+    for (let i = 0; i < allButtons.length; i++) {
+        const element = allButtons[i];
+        if (buttonClicked == element) {
+            buttonIndex = i;
+            break
+        }
+    }
+}
+
+
 // when page loads, any data saved in local storage shows on appropriate textarea
 
     // define this function then immediately call it 
     // go into localStorage.getItem("key name")
     // interact with specific textareas and if there is something returned from that key name then i want that data to appear in the textarea
 
-
-
-function saveReminder() {
-    localStorage.setItem('today', JSON.stringify(today));
+function showReminder() {
+    localStorage.getItem("data-hour");
 }
 
+// function saveReminder() {
+//     localStorage.setItem('today', JSON.stringify(today));
+// }
